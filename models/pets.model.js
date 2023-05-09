05633c70cbc1ec7bc7e9893aa7816b4102ef834a
Suraj Jaybhaye <suraj.jaybhaye@mitaoe.ac.in>
2023-05-09 23:16:43 +0530

    first commit

---
+++ models/pets.model.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose')
+
+const PetSchema = new mongoose.Schema({
+    petName: { type: 'String', required: true },
+    description: { type: 'String' },
+    age: { type: 'Number', required: true},
+    color: { type: 'String', required: true },
+    images: [{
+        type: String
+    }],
+    isAdopted: { type: 'Boolean', default: false }   
+},
+    {
+        collection: 'pets-data'
+    }
+)
+
+const model = mongoose.model('PetData', PetSchema)
+
+module.exports = model
